ea92bf40cfb6224aec1845f424bc4881c240c7ff
reasat-rafio <rafioar96@gmail.com>
2022-06-23 10:43:01 +0600

    :adhesive_bandage: build error fix

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -2,13 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Navbar } from "@components/common/navigation/navbar";
 import { Footer } from "@components/common/footer";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const is404Page = router.pathname === "/_error";
+
   return (
     <>
-      <Navbar {...pageProps.data?.site?.site} />
+      {!is404Page && <Navbar {...pageProps.data?.site?.site} />}
       <Component {...pageProps} />
-      <Footer {...pageProps.data?.site?.site} />
+      {!is404Page && <Footer {...pageProps.data?.site?.site} />}
     </>
   );
 }
