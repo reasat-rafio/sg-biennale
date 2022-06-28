89fb56b97d5384a102da15761fc9271fa8c10d25
reasat-rafio <rafioar96@gmail.com>
2022-06-28 06:25:38 +0600

    :construction: vistor info page query added

---
+++ apps/frontend/src/pages/visitor-info.tsx
@@ -0,0 +1,59 @@
+import { pageQuery } from "@lib/query";
+import { sanityStaticProps, useSanityQuery } from "@utils/sanity";
+import { GetStaticProps, GetStaticPropsContext, NextPage } from "next";
+import { groq } from "next-sanity";
+import { SanityProps } from "next-sanity-extra";
+import { renderObjectArray, withDimensions } from "sanity-react-extra";
+
+const query = pageQuery(groq`
+  *[_type == "homePage"][0]{
+    ...,
+    sections[]{
+        ..., 
+        shortGuide{
+            ...,
+            "icon": ${withDimensions("icon")}
+        },
+        moreInfos[]{
+            ...,
+            "icon": ${withDimensions("icon")}
+        },
+        additionalInfo[]{
+            ...,
+            "icon": ${withDimensions("icon")}
+        },
+        venues[]->{
+            ...,
+            "image": ${withDimensions("image")},
+            faqs[]{
+                ...,
+                answers[]{
+                    ...,
+                    "icon" :${withDimensions("icon")}
+                }
+            }
+        }
+    }
+  }
+`);
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => ({
+  props: await sanityStaticProps({ context, query }),
+  revalidate: 10,
+});
+
+const VisitorInfo: NextPage<SanityProps> = (props) => {
+  const { page } = useSanityQuery(query, props).data;
+
+  return (
+    <div>
+      {renderObjectArray(page.sections, {
+        // "visitorInfoPage.hero": "",
+      })}
+    </div>
+  );
+};
+
+export default VisitorInfo;
