ef469be965152a06a4f7a073c67276fd6cb7ddbc
reasat-rafio <rafioar96@gmail.com>
2022-07-19 14:30:17 +0600

    :art: tweak

--- apps/frontend/start.js
+++ apps/frontend/start.js
@@ -41,7 +41,7 @@ app.prepare().then(() => {
 
 const smee = new SmeeClient({
   source: "https://smee.io/uLUJvpIxZjvLLj7C",
-  target: "https://staging.singaporebiennale.org/",
+  target: "https://biennale4.singaporebiennale.org:8443/modules/git/public/web-hook.php?uuid=23c06c15-780d-1618-8c40-a4536b16e221",
   logger: console,
 });
 
