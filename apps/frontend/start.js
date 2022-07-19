c82cc4316c3cb41fa5b545ce442af5ec199aa2a9
reasat-rafio <rafioar96@gmail.com>
2022-07-19 16:18:41 +0600

    :test_tube: smee testing

--- apps/frontend/start.js
+++ apps/frontend/start.js
@@ -42,7 +42,7 @@ app.prepare().then(() => {
 const smee = new SmeeClient({
   source: "https://smee.io/uLUJvpIxZjvLLj7C",
   target:
-    "https://staging.singaporebiennale.org/modules/git/public/web-hook.php?uuid=23c06c15-780d-1618-8c40-a4536b16e221",
+    "https://staging.singaporebiennale.org:8443/modules/git/public/web-hook.php?uuid=23c06c15-780d-1618-8c40-a4536b16e221",
   logger: console,
 });
 
