48ab0c099bce0e8944e11fbb0cf77ec5a378709b
reasat-rafio <rafioar96@gmail.com>
2022-08-05 07:30:56 +0600

    :hammer: navbar tweak

--- apps/frontend/src/components/common/navigation/navbar.tsx
+++ apps/frontend/src/components/common/navigation/navbar.tsx
@@ -1,6 +1,7 @@
 import { ISite } from "@lib/@types/global.types";
 import { NavHeader } from "./nav-header";
 import { NavItems } from "./nav-items";
+import React from "react";
 
 export const Navbar: React.FC<ISite["site"]> = ({
   date,
@@ -10,7 +11,7 @@ export const Navbar: React.FC<ISite["site"]> = ({
   return (
     <nav id="navbar" className="lg:sticky top-0 left-0 z-40 bg-white">
       <NavHeader date={date} logo={logo} />
-      {/* <NavItems heightlights={heightlights} menu={menu} /> */}
+      <NavItems heightlights={heightlights} menu={menu} />
     </nav>
   );
 };
--- apps/frontend/src/components/icons/hamburger-menu.tsx
+++ apps/frontend/src/components/icons/hamburger-menu.tsx
@@ -1,3 +1,11 @@
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useState } from "react";
+
+const transition = {
+  type: "ease",
+  duration: 0.4,
+};
+
 interface HamburgerMenuProps {
   className?: string;
 }
@@ -5,20 +13,60 @@ interface HamburgerMenuProps {
 export const HamburgerMenu: React.FC<HamburgerMenuProps> = ({
   className = "h-6 w-6",
 }) => {
+  const [animate, setAnimation] = useState(false);
+
   return (
-    <svg
+    <motion.svg
+      className="cursor-pointer"
       width="24"
       height="20"
       viewBox="0 0 24 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      initial="initial"
+      animate="animate"
+      style={{ rotate: 180 }}
+      onMouseEnter={() => setAnimation(true)}
+      onMouseLeave={() => setAnimation(false)}
     >
       <rect width="24" height="3" fill="black" fill-opacity="0.3" />
-      <rect x="3" width="21" height="3" fill="black" />
+      <motion.rect
+        height="3"
+        fill="black"
+        initial={{
+          width: 17,
+        }}
+        animate={{
+          width: animate ? 24 : 17,
+          transition,
+        }}
+      />
       <rect y="9" width="24" height="3" fill="black" fill-opacity="0.3" />
-      <rect x="13" y="9" width="11" height="3" fill="black" />
+      <motion.rect
+        y="9"
+        height="3"
+        fill="black"
+        initial={{
+          width: 12,
+        }}
+        animate={{
+          width: animate ? 24 : 12,
+          transition,
+        }}
+      />
       <rect y="17" width="24" height="3" fill="black" fill-opacity="0.3" />
-      <rect x="7" y="17" width="17" height="3" fill="black" />
-    </svg>
+      <motion.rect
+        y="17"
+        height="3"
+        fill="black"
+        initial={{
+          width: 19,
+        }}
+        animate={{
+          width: animate ? 24 : 19,
+          transition,
+        }}
+      />
+    </motion.svg>
   );
 };
