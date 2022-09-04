a824c79988e18e90b009db6de421448f673b91b5
reasat-rafio <rafioar96@gmail.com>
2022-09-04 08:55:49 +0600

    :dizzy: page-header component animation added

--- apps/frontend/src/components/shared/page-heading.tsx
+++ apps/frontend/src/components/shared/page-heading.tsx
@@ -1,23 +1,84 @@
+import { motion, Variants } from "framer-motion";
+
 export interface PageHeaderProps {
   heading: string;
   tagline: string;
   color?: string;
 }
 
+const AnimationDuration = 0.7;
+const HeaderVariant: Variants = {
+  initial: {
+    translateY: "100%",
+    rotateX: "-95deg",
+    rotateY: 120,
+  },
+  animate: (delay: number) => ({
+    rotateX: 0,
+    translateY: 0,
+    rotateY: 0,
+    transition: {
+      delay: delay,
+      type: "tween",
+      ease: "easeInOut",
+      duration: AnimationDuration,
+    },
+  }),
+};
+const DescriptionVariants: Variants = {
+  initial: { opacity: 0 },
+  animate: (delay: number) => ({
+    opacity: 1,
+    transition: {
+      delay,
+      ease: "easeInOut",
+      type: "tween",
+      duration: AnimationDuration,
+    },
+  }),
+};
+
 export const PageHeading: React.FC<PageHeaderProps> = ({
   heading,
   tagline,
   color = "#F3F2EC",
 }) => {
+  const letters = Array.from(heading);
+  const headerAnimationDuration = (letters.length - 1) * 0.1 + 0.7;
+
   return (
     <header className="flex flex-col space-y-3 | xl:pt-xl md:pt-x pt-lg">
-      <h1 className="xl:text-heading-4 text-heading-5 font-medium">
-        {heading}
-      </h1>
-      <h2 className="max-w-2xl | text-gray--700 text-body-1 font-manrope | pb-4">
+      <motion.h1 className="overflow-hidden">
+        {letters.map((letter, index) => (
+          <motion.span
+            className="xl:text-heading-4 text-heading-5 font-medium inline-block whitespace-pre"
+            key={letter + index}
+            initial="initial"
+            animate="animate"
+            variants={HeaderVariant}
+            custom={index * 0.1}
+          >
+            {letter}
+          </motion.span>
+        ))}
+      </motion.h1>
+      <motion.h4
+        initial="initial"
+        animate="animate"
+        variants={DescriptionVariants}
+        custom={headerAnimationDuration}
+        className="max-w-2xl | text-gray--700 text-body-1 font-manrope | pb-4"
+      >
         {tagline}
-      </h2>
-      <span style={{ background: color }} className="h-[6px] w-[235px]" />
+      </motion.h4>
+      <motion.span
+        initial="initial"
+        animate="animate"
+        variants={DescriptionVariants}
+        custom={headerAnimationDuration + AnimationDuration}
+        style={{ background: color }}
+        className="h-[6px] w-[235px]"
+      />
     </header>
   );
 };
