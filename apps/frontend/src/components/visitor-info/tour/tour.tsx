4314679ebe73047e254e48ec58088dcc0098cf0d
reasat-rafio <rafioar96@gmail.com>
2022-09-06 18:28:48 +0600

    :iphone: tour section responsiveness added

--- apps/frontend/src/components/visitor-info/tour/tour.tsx
+++ apps/frontend/src/components/visitor-info/tour/tour.tsx
@@ -22,11 +22,11 @@ export const Tour: React.FC<TourProps> = ({
   return (
     <Container type="section" className="py-xxl">
       <h2 className="text-heading-4 font-medium">{header}</h2>
-      <div className="grid grid-cols-12 | mt-10 gap-10">
-        <figure className="col-span-8 h-[600px]">
+      <div className="grid grid-cols-12 | mt-10 lg:gap-10 gap-5">
+        <figure className="xl:col-span-8 lg:col-span-6 col-span-12 | 2xl:h-[600px] xl:h-[550px] lg:h-[480px] md:h-[450px] sm:h-[400px] h-[300px]">
           <ImageScene image={image} />
         </figure>
-        <div className="col-span-4 | font-manrope text-gray--700 text-body-1 | space-y-4 mt-auto">
+        <div className="xl:col-span-4 lg:col-span-6 col-span-12 | font-manrope text-gray--700 text-body-1 | space-y-4 mt-auto">
           <p>{description}</p>
           <Button variant="secondary">{cta.title}</Button>
         </div>
