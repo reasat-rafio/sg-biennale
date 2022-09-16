4249aeea404883865e18adbd31e6febf6077c189
reasat-rafio <rafioar96@gmail.com>
2022-09-16 07:36:02 +0600

    :dizzy: pasteditions card on hover mouse cursor follow animation added

--- apps/frontend/src/components/about/past-edition/card.tsx
+++ apps/frontend/src/components/about/past-edition/card.tsx
@@ -1,53 +1,87 @@
 import { PastEditionCollection } from "@lib/@types/about.types";
 import { useTransformSpring } from "@lib/helpers/animation.helpers";
 import { imageUrlBuilder } from "@utils/sanity";
-import { useMotionValue, motion } from "framer-motion";
+import { useMotionValue, motion, transform } from "framer-motion";
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import { MouseEvent, useRef, useState } from "react";
 import { SanityImg } from "sanity-react-extra";
 
 interface CardProps extends PastEditionCollection {
   index: number;
 }
-
+const physics = {
+  damping: 30,
+  stiffness: 60,
+  bounce: 0.1,
+  mass: 10,
+};
 export const Card: React.FC<CardProps> = ({ _key, image, title, url }) => {
   const router = useRouter();
   const cardRef = useRef<HTMLElement>(null);
-  // const [hoverYPosition, setHoverYPosition] = useState(0);
-  const scaleValue = useMotionValue(0);
-  const scale = useTransformSpring({
-    value: scaleValue,
-    inputRange: [0, 0.5],
-    outputRange: [-20, 20],
+  const [hovered, setHovered] = useState(false);
+  const screenX = useMotionValue(0);
+  const screenY = useMotionValue(0);
+  const _rotate = useMotionValue(0);
+
+  const x = useTransformSpring({
+    value: screenX,
+    outputRange: hovered ? [-100, 100] : [0, 0],
+    physics,
   });
+  const y = useTransformSpring({
+    value: screenY,
+    outputRange: hovered ? [-150, 150] : [0, 0],
+    physics,
+  });
+  const rotate = useTransformSpring({
+    value: _rotate,
+    outputRange: hovered ? [-10, 10] : [0, 0],
+    physics,
+  });
+
+  function handleMouseMove(event: MouseEvent<HTMLElement>) {
+    const width = transform([0, window.innerWidth], [0, 1])(event.clientX);
+    const height = transform([0, window.innerHeight], [0, 1])(event.clientY);
+    const rotateX = transform(
+      [0, window.innerWidth + window.innerHeight],
+      [0, 1]
+    )(event.clientX + event.clientY);
+
+    screenX.set(width);
+    screenY.set(height);
+    _rotate.set(rotateX);
+  }
 
   return (
     <article
       key={_key}
       ref={cardRef}
-      className="flex w-[calc(25%-2%)] flex-col | space-y-4 p-5 | bg-white | rounded | cursor-pointer "
-      onMouseMove={({ clientY }) => {
-        if (typeof window !== "undefined") {
-          const y = -(clientY / window.innerHeight) * 2 + 1;
-          scaleValue.set(y);
-        }
+      className="flex w-[calc(25%-2%)] flex-col | space-y-4 p-5 | bg-white | rounded | cursor-pointer overflow-hidden"
+      onMouseMove={handleMouseMove}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => {
+        x.set(0);
+        y.set(0);
+        setHovered(false);
       }}
       onClick={() => {
         if (url) router.push(url);
       }}
     >
       <motion.figure
-        style={{ translateX: scale }}
+        style={{
+          x,
+          y,
+          rotate,
+        }}
         className="max-h-[350px] p-7 overflow-hidden"
       >
-        <motion.span className="h-full w-full">
-          <SanityImg
-            width={400}
-            image={image}
-            builder={imageUrlBuilder}
-            className="h-full w-full object-cover"
-          />
-        </motion.span>
+        <SanityImg
+          width={400}
+          image={image}
+          builder={imageUrlBuilder}
+          className="h-full w-full object-cover"
+        />
       </motion.figure>
       <section>
         <h6 className="text-lg font-medium mb-1 font-manrope">{title}</h6>
