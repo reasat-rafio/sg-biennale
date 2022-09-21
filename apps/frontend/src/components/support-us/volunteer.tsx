dbb1182895f5a6c428d620e5e3f81fa1a27f2a1a
reasat-rafio <rafioar96@gmail.com>
2022-09-21 16:28:32 +0600

    :wastebasket: cleanup

--- apps/frontend/src/components/support-us/volunteer.tsx
+++ apps/frontend/src/components/support-us/volunteer.tsx
@@ -2,13 +2,8 @@ import { Button } from "@components/ui/button";
 import { Container } from "@components/ui/container";
 import { Cta } from "@lib/@types/global.types";
 import { SanityImage, SanityImg } from "sanity-react-extra";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { imageUrlBuilder } from "@utils/sanity";
 import React from "react";
-import "keen-slider/keen-slider.min.css";
-import { useKeenSlider } from "keen-slider/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
@@ -18,37 +13,6 @@ interface VolunteerProps {
   cta: Cta;
   images: SanityImage[];
 }
-
-const settings = {
-  speed: 1000,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  initialSlide: 0,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        initialSlide: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-};
 
 export const Volunteer: React.FC<VolunteerProps> = ({
   header,
@@ -101,18 +65,6 @@ export const Volunteer: React.FC<VolunteerProps> = ({
           </SwiperSlide>
         ))}
       </Swiper>
-      {/* <Slider className="lg:my-8 my-5" {...settings}>
-        {images.map((image) => (
-          <div className="p-5 h-[330px] outline-none" key={image._key}>
-            <SanityImg
-              className="w-full h-full object-cover"
-              width={400}
-              image={image}
-              builder={imageUrlBuilder}
-            />
-          </div>
-        ))}
-      </Slider> */}
     </Container>
   );
 };
