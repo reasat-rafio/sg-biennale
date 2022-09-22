1f2ce61725dd87866d40dbf1826472add5effa4c
reasat-rafio <rafioar96@gmail.com>
2022-09-22 18:39:12 +0600

    :hammer: filter by category added

--- apps/frontend/src/components/programmes-and-events/filter/filter-by-category.tsx
+++ apps/frontend/src/components/programmes-and-events/filter/filter-by-category.tsx
@@ -1,36 +1,16 @@
 import useProgrammesAndEventsStore from "@stores/programme-event-store";
-import clsx from "clsx";
-import { filterHeaderStyles } from "./filters";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { AllCategoriesProps } from "@lib/@types/programmes-events-types";
 import { useRouter } from "next/router";
+import { Listbox, Transition } from "@headlessui/react";
 
-interface FilterByCategoryProps {
-  className?: string;
-}
-
-export const FilterByCategory: React.FC<FilterByCategoryProps> = ({
-  className,
-}) => {
+export const FilterByCategory: React.FC<{}> = () => {
   const router = useRouter();
-
   const { allCategories } = useProgrammesAndEventsStore();
   const [selectedCategory, setSelectedCategory] = useState<null | string>(null);
 
-  const onClickAction = (_category: AllCategoriesProps) => {
-    setSelectedCategory(_category._id);
-    // ✅ setting the filter query
-    router.push(
-      { query: { ...router.query, category: _category?.slug.current } },
-      undefined,
-      {
-        shallow: true,
-      }
-    );
-  };
-
-  /* 
-  ❓Getting the query from the url and setting them to the state 
+  /*
+  ❓Getting the query from the url and setting them to the state
   @REASON: This stop the state to reset after refresh
   */
   useEffect(() => {
@@ -41,31 +21,87 @@ export const FilterByCategory: React.FC<FilterByCategoryProps> = ({
       const [findThecategory] = allCategories.filter(
         ({ slug }) => slug.current === selectedCategoryFromUrlQuery
       );
-      setSelectedCategory(findThecategory?._id);
+      setSelectedCategory(findThecategory?.name);
     }
   }, [router, allCategories]);
 
+  const onClickAction = (_category: AllCategoriesProps) => {
+    setSelectedCategory(_category.name);
+    // ✅ setting the filter query
+    router.push(
+      { query: { ...router.query, category: _category?.slug.current } },
+      undefined,
+      {
+        shallow: true,
+      }
+    );
+  };
   return (
-    <div className={clsx(className)}>
-      <h6 className={filterHeaderStyles}>Category</h6>
-
-      <ul className="grid grid-cols-12 | gap-2">
-        {allCategories.map(({ _id, name, slug }) => (
-          <li
-            className="col-span-3 | cursor-pointer"
-            key={_id}
-            onClick={() => onClickAction({ _id, name, slug })}
+    <div className="w-72 z-20">
+      <Listbox value={selectedCategory}>
+        <div className="relative mt-1">
+          <Listbox.Button className="flex items-center w-[300px] | space-x-4 p-3 | border-b border-black cursor-pointer">
+            <span className="flex-1 text-left block truncate">
+              {selectedCategory ? selectedCategory : "Category"}
+            </span>
+            <img className="w-[18px]" src="/icons/sort.svg" alt="sort icon" />
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <input
-              checked={selectedCategory === _id}
-              type="radio"
-              value={name}
-              name="gender"
-            />
-            <span> {name}</span>
-          </li>
-        ))}
-      </ul>
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm | scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100 ">
+              {allCategories.map((person, personIdx) => (
+                <Listbox.Option
+                  key={personIdx}
+                  onClick={() => onClickAction(person)}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-vulcanic text-gray--700" : "text-gray-900"
+                    }`
+                  }
+                  value={person}
+                >
+                  {() => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          person.name === selectedCategory
+                            ? "font-medium"
+                            : "font-normal"
+                        }`}
+                      >
+                        {person.name}
+                      </span>
+                      {person.name === selectedCategory ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-red-love">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="h-5 w-5"
+                            aria-hidden="true"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M4.5 12.75l6 6 9-13.5"
+                            />
+                          </svg>
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
     </div>
   );
 };
--- apps/frontend/src/components/programmes-and-events/filter/filtering-section.tsx
+++ apps/frontend/src/components/programmes-and-events/filter/filtering-section.tsx
@@ -1,6 +1,8 @@
 import { ChevronArrow } from "@components/icons/chevron-arrow";
 import useProgrammesAndEventsStore from "@stores/programme-event-store";
 import { useState } from "react";
+import { FilterByCategory } from "./filter-by-category";
+import { FilterByVenue } from "./filter-by-venue";
 import { Filters } from "./filters";
 
 interface FilteringSectionProps {}
@@ -13,7 +15,12 @@ export const FilteringSection: React.FC<FilteringSectionProps> = ({}) => {
       <header>
         <h2 className="font-medium text-heading-6">All Events</h2>
       </header>
-      <div className=""></div>
+      <div className="flex space-x-5">
+        <div className="flex-1">{/* <FilterByVenue /> */}</div>
+        <div className="flex ">
+          <FilterByCategory />
+        </div>
+      </div>
     </section>
   );
 };
