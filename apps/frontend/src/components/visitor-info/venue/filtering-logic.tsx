09e052bd30bc27c3def3ce002974148b09e95cbb
reasat-rafio <rafioar96@gmail.com>
2022-09-27 01:15:29 +0600

    :construction: venue filtering logic added

--- apps/frontend/src/components/visitor-info/venue/filtering-logic.tsx
+++ apps/frontend/src/components/visitor-info/venue/filtering-logic.tsx
@@ -1,9 +1,38 @@
-import { ReactNode } from "react";
+import useVisitorInfoStore from "@stores/visitor-info.store";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect } from "react";
 
 interface FilteringLogicProps {
   children: ReactNode;
 }
 
+/* 🚩 gatekeeper of the intended queries */
+const checkerForTheIntendedQuery = (filteringKeys: string[]) => {
+  return filteringKeys.includes("search") || filteringKeys.includes("sort_by");
+};
+
 export const FilteringLogic: React.FC<FilteringLogicProps> = ({ children }) => {
+  const router = useRouter();
+
+  const {
+    page,
+    cardsPerPage,
+    searchInput,
+    allVenues,
+    setPage,
+    setSortedVenues,
+  } = useVisitorInfoStore();
+
+  useEffect(() => {
+    /* 🚩 check if the qury field is not empty */
+    const queryParamsNotEmpty = JSON.stringify(router.query) !== "{}";
+    const queryKeys = Object.keys(router.query);
+
+    if (queryParamsNotEmpty && checkerForTheIntendedQuery(queryKeys)) {
+      const selectedSorting = router.query.sort_by;
+      const _searchInput = router.query.search;
+    }
+  }, [page, router.query, allVenues, setSortedVenues]);
+
   return <>{children}</>;
 };
--- apps/frontend/src/stores/visitor-info.store.ts
+++ apps/frontend/src/stores/visitor-info.store.ts
@@ -1,3 +1,4 @@
+import { VenueProps } from "@lib/@types/visitor-info.types";
 import create from "zustand";
 import { devtools } from "zustand/middleware";
 
@@ -5,16 +6,26 @@ interface VisitorInfoStoreProps {
   page: number;
   cardsPerPage: number;
   searchInput: string | null;
+  allVenues: VenueProps[];
+  sortedVenues: VenueProps[];
   setPage: (data: number) => void;
   setSearchInput: (data: string | null) => void;
+  setSortedVenues: (data: VenueProps[]) => void;
+  setAllVenues: (data: VenueProps[]) => void;
 }
 
 const useVisitorInfoStore = create(
   devtools<VisitorInfoStoreProps>((set) => ({
     page: 1,
+    allVenues: [],
+    sortedVenues: [],
     cardsPerPage: 6,
     searchInput: null,
     setPage: (page) => set((state) => ({ ...state, page })),
+    setAllVenues: (allVenues: VenueProps[]) =>
+      set((state) => ({ ...state, allVenues })),
+    setSortedVenues: (sortedVenues: VenueProps[]) =>
+      set((state) => ({ ...state, sortedVenues })),
     setSearchInput: (searchInput: string | null) =>
       set((state) => ({ ...state, searchInput })),
   }))
