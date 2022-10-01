a7952f1527d2af7e576ae4aa2150d19be4a83b13
reasat-rafio <rafioar96@gmail.com>
2022-10-01 23:43:51 +0600

    :construction_worker: build

--- apps/frontend/src/lib/query.ts
+++ apps/frontend/src/lib/query.ts
@@ -12,7 +12,7 @@ export const siteQuery = groq`{
       },
       navigations {
         ...,
-        cta[] {
+        ctas[] {
           ...,
           "icon": ${withDimensions("icon")},
         },
