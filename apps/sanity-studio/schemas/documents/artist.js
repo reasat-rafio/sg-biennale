68b08baf9811877ad28bbf1c1889978b4828e98d
reasat-rafio <rafioar96@gmail.com>
2022-10-05 13:48:58 +0600

    :recycle: artist schema tweak

--- apps/sanity-studio/schemas/documents/artist.js
+++ apps/sanity-studio/schemas/documents/artist.js
@@ -64,6 +64,20 @@ const Artist = {
       },
       validation: (Rule) => Rule.required(),
     },
+
+    {
+      name: "region",
+      type: "tag",
+      options: {
+        predefinedTags: [
+          { label: "Singapore", value: "singapore" },
+          { label: "Rest of the Asia", value: "rest-of-the-asia" },
+          { label: "Rest of the world", value: "rest-of-the-world" },
+        ],
+        allowCreate: false,
+      },
+      validation: (Rule) => Rule.required(),
+    },
   ],
   preview: {
     select: {
