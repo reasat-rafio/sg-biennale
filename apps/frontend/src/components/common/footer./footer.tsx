b736c97386076204acac98ca829b1a207a1a49e4
reasat-rafio <rafioar96@gmail.com>
2022-10-12 22:16:39 +0600

    :white_check_mark: footer adjustment complete

--- apps/frontend/src/components/common/footer./footer.tsx
+++ apps/frontend/src/components/common/footer./footer.tsx
@@ -1,24 +1,28 @@
 import { ISite } from "@lib/@types/global.types";
-import { useWindowSize } from "@lib/hooks";
+import { getShuffledArr } from "@lib/helpers/global.helpers";
 import { imageUrlBuilder } from "@utils/sanity";
-import clsx from "clsx";
-import { SanityImg } from "sanity-react-extra";
+import { useEffect, useState } from "react";
+import { SanityImg, SanityImage } from "sanity-react-extra";
 import { Address } from "./address";
 import { NewsLetter } from "./news-letter";
-import Styles from "@styles/foooter.module.css";
 
 export const Footer: React.FC<ISite["site"]> = ({
   date,
-  footer: { header, image, location, newsLetter, social },
+  kvs,
+  footer: { header, location, newsLetter, social },
 }) => {
-  const windowWidth = useWindowSize()?.width ?? 0;
+  const [suffledKVs, setSuffledKVs] = useState<SanityImage[]>(kvs[0]);
+
+  useEffect(() => {
+    setSuffledKVs(getShuffledArr(kvs));
+  }, []);
 
   return (
     <footer
       id="footer"
-      className="relative | grid grid-cols-12 | xl:pb-14 xl:px-0 lg:px-x sm:px-lg px-md py-10"
+      className="relative | grid grid-cols-12 | sm:gap-10 gap-5 xl:pb-14 xl:px-0 lg:px-x sm:px-lg px-md py-10 xl:pl-max"
     >
-      <section className="relative z-10 | flex flex-col justify-center items-center col-span-12 lg:col-span-6 xl:col-span-5 | xl:space-y-20 xl:pl-max">
+      <section className="col-span-12 lg:col-span-5 flex flex-col justify-center items-center | xl:space-y-20">
         <header className="w-full | font-semibold">
           <h4 className=" mb-4 | lg:text-left text-center | whitespace-pre-wrap | lg:text-heading-4 text-heading-6">
             {header}
@@ -28,28 +32,17 @@ export const Footer: React.FC<ISite["site"]> = ({
         <NewsLetter {...newsLetter} />
         <Address social={social} location={location} />
       </section>
-
-      <figure className="relative | col-span-12 lg:col-span-6 xl:col-span-7 | flex justify-center items-center">
-        <SanityImg
-          className="w-2/3 object-cover z-10 relative"
-          width={1080}
-          image={image}
-          builder={imageUrlBuilder}
-          alt={`${header}' image`}
-        />
-      </figure>
-
-      {/* Background */}
-      <div className="bg-[url('/bg/footer-background-decor.svg')] h-full w-full absolute top-0 left-0 bg-repeat" />
-      {/* Faded gardient */}
-      <span
-        className={clsx(
-          "absolute top-0 left-0 h-full w-full",
-          windowWidth >= 1024
-            ? Styles.footer_bg_gradient_left_to_right
-            : Styles.footer_bg_gradient_top_to_bottom
-        )}
-      />
+      <section className="col-span-12 xl:col-span-6 lg:col-span-7 flex justify-center items-center max-h-[700px] ">
+        <figure className="h-full xl:w-auto w-full object-contain | xl:p-[10%] p-[5%] | bg-[url('/bg/footer-background-decor.png')] bg-repeat  | border border-black bg-contain">
+          <SanityImg
+            className="object-center h-full w-full object-contain"
+            width={500}
+            image={suffledKVs[0] ?? kvs[0]}
+            builder={imageUrlBuilder}
+            alt={`${header}' image`}
+          />
+        </figure>
+      </section>
     </footer>
   );
 };
