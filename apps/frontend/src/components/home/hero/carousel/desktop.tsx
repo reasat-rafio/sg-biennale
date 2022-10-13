b42ca8f406ec733e858b7d00135718de960bcac0
reasat-rafio <rafioar96@gmail.com>
2022-10-13 16:32:37 +0600

    :hammer: hero section tweak

--- apps/frontend/src/components/home/hero/carousel/desktop.tsx
+++ apps/frontend/src/components/home/hero/carousel/desktop.tsx
@@ -3,6 +3,7 @@ import { SanityImg, SanityImage } from "sanity-react-extra";
 import { imageUrlBuilder } from "@utils/sanity";
 import { Autoplay } from "swiper";
 import clsx from "clsx";
+import { motion } from "framer-motion";
 
 interface DesktopProps {
   suffledKVs: SanityImage[];
@@ -17,31 +18,35 @@ export const Desktop: React.FC<DesktopProps> = ({ suffledKVs }) => {
       modules={[Autoplay]}
       autoplay={{ delay: 4000 }}
       slidesPerView="auto"
-      spaceBetween={20}
+      spaceBetween={40}
     >
       {suffledKVs.map((image, index) => (
         <SwiperSlide
           className={clsx(
             "relative flex justify-center items-center",
-            index === 0 && "w-[40vw] mt-[5%] p-16",
-            index === 1 && "w-[15vw] p-5",
-            index === 2 && "w-[20vw] mt-[25%] -translate-x-[10%] p-10",
-            index === 3 && "w-[35vw] mt-[2%] p-14"
+            index === 0 && "w-[40vw] mt-[5%]",
+            index === 1 && "w-[15vw]",
+            index === 2 && "w-[20vw] mt-[25%] -translate-x-[10%]",
+            index === 3 && "w-[35vw] mt-[2%]"
           )}
           key={image._key}
         >
-          <figure className="w-full relative z-10">
+          <motion.figure
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="w-full relative z-10"
+          >
             <SanityImg
               className="h-full w-full | object-contain"
               image={image}
               builder={imageUrlBuilder}
-              width={500}
+              width={600}
             />
-          </figure>
+          </motion.figure>
 
           <div
             className={clsx(
-              "absolute top-0 left-0  w-full bg-[#F8F8F8] z-0",
+              "absolute top-0 left-0 w-full z-0",
               image.shortBackground ? "h-[65%]" : "h-full"
             )}
           />
