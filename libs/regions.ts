5639ca3ff313ed64727837f9fdecd52f521bad3b
reasat-rafio <rafioar96@gmail.com>
2022-10-13 12:23:02 +0600

    :recycle: regions name twaek

--- libs/regions.ts
+++ libs/regions.ts
@@ -1,6 +1,6 @@
 const regions = [
   { label: "Singapore", value: "singapore" },
-  { label: "Rest of the Asia", value: "rest-of-the-asia" },
+  { label: "Rest of Asia", value: "rest-of-asia" },
   { label: "Rest of the world", value: "rest-of-the-world" },
 ];
 
