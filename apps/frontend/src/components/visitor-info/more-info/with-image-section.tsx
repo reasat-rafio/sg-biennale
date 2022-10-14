0191430ba71277bcc266d432ad3ae349285a978b
reasat-rafio <rafioar96@gmail.com>
2022-10-14 20:10:00 +0600

    :recycle: visitor info image following mouse cursor animation remove

--- apps/frontend/src/components/visitor-info/more-info/with-image-section.tsx
+++ apps/frontend/src/components/visitor-info/more-info/with-image-section.tsx
@@ -33,49 +33,49 @@ export const WithImageSection: React.FC<WithImageSectionProps> = ({
 }) => {
   const router = useRouter();
   const windowWidth = useWindowSize()?.width ?? 0;
-  const [hovered, setHovered] = useState(false);
-  const screenX = useMotionValue(0);
-  const screenY = useMotionValue(0);
-  const _rotate = useMotionValue(0);
+  // const [hovered, setHovered] = useState(false);
+  // const screenX = useMotionValue(0);
+  // const screenY = useMotionValue(0);
+  // const _rotate = useMotionValue(0);
 
-  const x = useTransformSpring({
-    value: screenX,
-    outputRange: hovered ? [-100, 100] : [0, 0],
-    physics,
-  });
-  const y = useTransformSpring({
-    value: screenY,
-    outputRange: hovered ? [-100, 100] : [0, 0],
-    physics,
-  });
-  const rotate = useTransformSpring({
-    value: _rotate,
-    outputRange: hovered ? [-10, 15] : [0, 0],
-    physics,
-  });
+  // const x = useTransformSpring({
+  //   value: screenX,
+  //   outputRange: hovered ? [-100, 100] : [0, 0],
+  //   physics,
+  // });
+  // const y = useTransformSpring({
+  //   value: screenY,
+  //   outputRange: hovered ? [-100, 100] : [0, 0],
+  //   physics,
+  // });
+  // const rotate = useTransformSpring({
+  //   value: _rotate,
+  //   outputRange: hovered ? [-10, 15] : [0, 0],
+  //   physics,
+  // });
 
-  function handleMouseMove(event: MouseEvent<HTMLElement>) {
-    const width = transform([0, window.innerWidth], [0, 1])(event.clientX);
-    const height = transform([0, window.innerHeight], [0, 1])(event.clientY);
-    const rotateX = transform(
-      [0, window.innerWidth + window.innerHeight],
-      [0, 1]
-    )(event.clientX + event.clientY);
+  // function handleMouseMove(event: MouseEvent<HTMLElement>) {
+  //   const width = transform([0, window.innerWidth], [0, 1])(event.clientX);
+  //   const height = transform([0, window.innerHeight], [0, 1])(event.clientY);
+  //   const rotateX = transform(
+  //     [0, window.innerWidth + window.innerHeight],
+  //     [0, 1]
+  //   )(event.clientX + event.clientY);
 
-    screenX.set(width);
-    screenY.set(height);
-    _rotate.set(rotateX);
-  }
+  //   screenX.set(width);
+  //   screenY.set(height);
+  //   _rotate.set(rotateX);
+  // }
 
   return (
     <motion.article
-      onMouseMove={handleMouseMove}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => {
-        x.set(0);
-        y.set(0);
-        setHovered(false);
-      }}
+      // onMouseMove={handleMouseMove}
+      // onMouseEnter={() => setHovered(true)}
+      // onMouseLeave={() => {
+      //   x.set(0);
+      //   y.set(0);
+      //   setHovered(false);
+      // }}
       className="grid grid-cols-12 col-span-12 justify-center items-center | py-10 lg:gap-10 gap-5"
     >
       <section className="xl:col-span-7 md:col-span-6 col-span-12 | space-y-6">
@@ -96,11 +96,11 @@ export const WithImageSection: React.FC<WithImageSectionProps> = ({
       </section>
       <motion.figure
         className="xl:col-span-5 md:col-span-6 col-span-12 -z-20"
-        style={{
-          x,
-          y,
-          rotate,
-        }}
+        // style={{
+        //   x,
+        //   y,
+        //   rotate,
+        // }}
       >
         <SanityImg
           className="h-full w-full object-cover"
