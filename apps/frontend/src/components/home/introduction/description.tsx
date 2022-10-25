97a706a6fec5fce38901de219d76f0c17fe08ee9
reasat-rafio <rafioar96@gmail.com>
2022-10-25 17:29:19 +0600

    :dizzy: home page itroduction section on scroll skew animation remove

--- apps/frontend/src/components/home/introduction/description.tsx
+++ apps/frontend/src/components/home/introduction/description.tsx
@@ -1,6 +1,6 @@
-import { useIntersection, useWindowSize } from "@lib/hooks";
-import { useEffect, useRef } from "react";
-import { motion, useMotionValue, useSpring, Variants } from "framer-motion";
+import { useIntersection } from "@lib/hooks";
+import { useRef } from "react";
+import { motion, Variants } from "framer-motion";
 
 interface DescriptionProps {
   subtitle: string;
@@ -16,50 +16,18 @@ const LetterVariant: Variants = {
   },
 };
 
-const skewConfig = { ease: 0.1, current: 0, previous: 0, rounded: 0 };
-
 export const Description: React.FC<DescriptionProps> = ({
   subtitle,
   description,
 }) => {
-  const windowWidth = useWindowSize()?.width ?? 0;
   const containerRef = useRef<HTMLHeadingElement>(null);
   const subtitleRef = useRef<HTMLHeadingElement>(null);
   const intersection = useIntersection(subtitleRef, { threshold: 0.25 });
-
-  const motionSkew = useMotionValue(0);
-  const animatedMotionSkewY = useSpring(motionSkew, { damping: 15 });
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const skewScrolling = () => {
-        //Set Current to the scroll position amount
-        skewConfig.current = window.scrollY;
-        // Set Previous to the scroll previous position
-        skewConfig.previous +=
-          (skewConfig.current - skewConfig.previous) * skewConfig.ease;
-        // Set rounded to
-        skewConfig.rounded = Math.round(skewConfig.previous * 100) / 100;
-
-        // Difference between
-        const difference = skewConfig.current - skewConfig.rounded;
-        const acceleration = difference / windowWidth;
-        const velocity = +acceleration;
-        const skew = velocity * 5;
-        motionSkew.set(skew);
-      };
-
-      window.addEventListener("scroll", () => {
-        skewScrolling();
-      });
-    }
-  }, [windowWidth]);
 
   const letters = Array.from(subtitle);
   return (
     <motion.div
       ref={containerRef}
-      style={{ skewY: animatedMotionSkewY }}
       className="flex flex-col space-y-8 max-w-7xl mx-auto pt-xl"
     >
       <motion.h4
