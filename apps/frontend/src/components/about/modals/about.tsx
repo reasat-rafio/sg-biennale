ad77459fbcea6c2c9e085f71c0e33dd8ed0b02eb
reasat-rafio <rafioar96@gmail.com>
2022-11-03 15:58:08 +0600

    :hammer: about store adjustment

---
+++ apps/frontend/src/components/about/modals/about.tsx
@@ -0,0 +1,7 @@
+import { Portal } from "@reach/portal";
+
+interface AboutProps {}
+
+export const About: React.FC<AboutProps> = ({}) => {
+  return <Portal></Portal>;
+};
--- apps/frontend/src/stores/about.store.ts
+++ apps/frontend/src/stores/about.store.ts
@@ -1,32 +1,49 @@
-import { PastEditionCollection, TeamCollection } from "@lib/@types/about.types";
+import {
+  PastEditionCollection,
+  SponsorCollection,
+  TeamCollection,
+} from "@lib/@types/about.types";
 import create from "zustand";
 import { devtools } from "zustand/middleware";
 
 interface AboutStoreProps {
   pastEditions: PastEditionCollection[];
+  abouts: SponsorCollection[];
   coArtisticDirectors: TeamCollection[];
   selectedPastEditionId: string | null;
+  selectedAboutId: string | null;
   selectedCoArtisticDirectorId: string | null;
   setSelectedPastEditionId: (data: string | null) => void;
+  setSelectedAboutId: (data: string | null) => void;
   setSelectedCoArtisticDirectorId: (data: string | null) => void;
   setPastEditions: (data: PastEditionCollection[]) => void;
+  setAbouts: (data: SponsorCollection[]) => void;
   setCoArtisticDirectors: (data: TeamCollection[]) => void;
 }
 
 const useAboutStore = create(
   devtools<AboutStoreProps>((set) => ({
     pastEditions: [],
+    abouts: [],
     coArtisticDirectors: [],
     selectedPastEditionId: null,
+    selectedAboutId: null,
     selectedCoArtisticDirectorId: null,
     setSelectedPastEditionId: (selectedPastEditionId) =>
       set((state) => ({ ...state, selectedPastEditionId })),
+    setSelectedAboutId: (selectedAboutId) =>
+      set((state) => ({ ...state, selectedAboutId })),
     setSelectedCoArtisticDirectorId: (selectedCoArtisticDirectorId) =>
       set((state) => ({ ...state, selectedCoArtisticDirectorId })),
     setPastEditions: (pastEditions) =>
       set((state) => ({
         ...state,
         pastEditions,
+      })),
+    setAbouts: (abouts) =>
+      set((state) => ({
+        ...state,
+        abouts,
       })),
     setCoArtisticDirectors: (coArtisticDirectors) =>
       set((state) => ({
