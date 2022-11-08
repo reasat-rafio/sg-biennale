1cfa4478533486a48cbf9d5c13997456cd1c5d3a
reasat-rafio <rafioar96@gmail.com>
2022-11-09 00:57:27 +0600

    :adhesive_bandage: build error fix

--- apps/frontend/src/components/about/modals/index.tsx
+++ apps/frontend/src/components/about/modals/index.tsx
@@ -1,14 +1,16 @@
 import dynamic from "next/dynamic";
-const About = dynamic(() => import("./about").then((com) => com.About), {
+const About = dynamic<{}>(() => import("./about").then((com) => com.About), {
   ssr: false,
 });
-const PastEdition = dynamic(
+const PastEdition = dynamic<{}>(
   () => import("./past-edition").then((com) => com.PastEdition),
   { ssr: false }
 );
-const CoArtistDirector = dynamic(
+const CoArtistDirector = dynamic<{}>(
   () => import("./co-artist-director").then((com) => com.CoArtistDirector),
-  { ssr: false }
+  {
+    ssr: false,
+  }
 );
 
 export const Modals: React.FC<{}> = ({}) => (
