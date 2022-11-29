2a8788b965a44df2cffecc4c83e60bf58ede490e
reasat-rafio <rafioar96@gmail.com>
2022-11-29 06:50:37 +0600

    :closed_lock_with_key: csp tweak

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -10,13 +10,13 @@ const STUDIO_REWRITE = {
 
 const ContentSecurityPolicy = `
   default-src 'self' 'unsafe-inline' 'unsafe-eval' *.userway.org cdn.sanity.io https://66wl3wil.api.sanity.io;
-  script-src 'self' 'unsafe-inline' 'unsafe-eval'  https://www.google-analytics.com https://www.googletagmanager.com https://connect.facebook.net;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com https://www.googletagmanager.com https://www.facebook.com https://connect.facebook.net;
   child-src 'none';
   font-src 'self' fonts.gstatic.com  data:;
   style-src 'self' 'unsafe-inline' fonts.googleapis.com; 
   base-uri 'self';
   frame-ancestors 'self';
-  frame-src 'self' https://www.youtube.com https://www.onemap.gov.sg;
+  frame-src 'self' https://www.youtube.com https://www.onemap.gov.sg www.facebook.com;
   img-src 'self' cdn.sanity.io 66wl3wil.api.sanity.io https://www.google.com https://www.google-analytics.com https://stats.g.doubleclick.net; 
   connect-src 'self' https://66wl3wil.api.sanity.io https://www.google-analytics.com https://www.google-analytics.com https://stats.g.doubleclick.net;
   media-src 'self' cdn.sanity.io;
