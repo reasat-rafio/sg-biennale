363050e8a80bf0d52a2cbb68f04ca8d0eb229c9d
reasat-rafio <rafioar96@gmail.com>
2022-12-16 14:01:45 +0600

    :recycle: artist page carousel rework with less animation and freemode

--- apps/frontend/src/components/artist-details/programmes-events/programmes-events-carousel.tsx
+++ apps/frontend/src/components/artist-details/programmes-events/programmes-events-carousel.tsx
@@ -1,48 +1,50 @@
+import "swiper/css";
+import "swiper/css/free-mode";
+import clsx from "clsx";
 import { Backside } from "@components/programmes-and-events/closest-event/backside";
 import { FrontSide } from "@components/programmes-and-events/closest-event/font-side";
 import { RelatedEventsProps } from "@lib/@types/artist-details.types";
 import { useWindowSize } from "@lib/hooks";
-import { motion, Point } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Swiper as SwiperType, FreeMode } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 interface ProgrammesEventsCarouselProps {
   relatedEvents: RelatedEventsProps[];
 }
-const swipeConfidenceThreshold = 1500;
-const swipePower = (offset: number, velocity: number) => {
-  return Math.abs(offset) * velocity;
-};
-
-const transition = (index: number) => ({
-  left: {
-    type: "tween",
-    duration: 0.6,
-    ease: "easeInOut",
-  },
-  scale: {
-    delay: index * 0.2,
-    type: "tween",
-    duration: 0.5,
-    ease: "easeInOut",
-  },
-});
 
 export const ProgrammesEventsCarousel: React.FC<
   ProgrammesEventsCarouselProps
 > = ({ relatedEvents }) => {
-  const carouselRef = useRef<HTMLElement | null>(null);
   const windowWidth = useWindowSize()?.width ?? 0;
-  const [position, setPosition] = useState(1);
-  const [activeCardIndex, setActiveCardIndex] = useState<number | null>(null);
-  const [disableSwipingRight, setDisableSwipingRight] =
-    useState<boolean>(false);
-  const [disableSwipingLeft, setDisableSwipingLeft] = useState<boolean>(false);
+  const swiperRef = useRef<SwiperType>();
+  const [activeCard, setActiveCard] = useState<number | null>(null);
   const [cardsPerView, setCardsperView] = useState(3);
+  const sectionRef = useRef<HTMLElement>(null);
+  const cardsWidth = useMemo(
+    () =>
+      sectionRef?.current &&
+      sectionRef?.current?.getBoundingClientRect().width / cardsPerView,
+    [cardsPerView, windowWidth, sectionRef]
+  );
+  const spaceBetween = useMemo(
+    () => (cardsPerView > 2 ? 40 : 30),
+    [cardsPerView]
+  );
 
-  const fullWidth = (1 / cardsPerView) * 2 * 100;
-  const halfWidth = (1 / cardsPerView) * 100;
-  const oneCardInView = cardsPerView === 1;
-  const gapInPixel = windowWidth >= 1024 ? 25 : windowWidth >= 720 ? 15 : 0;
+  const onClickCardAction = (index: number) =>
+    setActiveCard((prev) => (prev === index ? null : index));
+
+  const getWidth = useCallback(
+    (index: number) =>
+      index === activeCard
+        ? cardsPerView > 1
+          ? cardsWidth! * 2 - spaceBetween / 2
+          : cardsWidth! - spaceBetween / 2
+        : cardsWidth! - spaceBetween / 2,
+    [activeCard, cardsWidth, cardsPerView]
+  );
 
   useEffect(() => {
     if (windowWidth >= 1280) setCardsperView(3);
@@ -51,117 +53,82 @@ export const ProgrammesEventsCarousel: React.FC<
     else setCardsperView(1);
   }, [windowWidth, setCardsperView]);
 
-  const paginate = (newDirection: number) => {
-    setPosition(position + newDirection);
-  };
-
-  const onDragAction = (offset: Point, velocity: Point) => {
-    const swipe = swipePower(offset.x, velocity.x);
-    if (swipe < -swipeConfidenceThreshold && !disableSwipingLeft) {
-      paginate(1);
-    } else if (swipe > swipeConfidenceThreshold && !disableSwipingRight) {
-      paginate(-1);
+  useEffect(() => {
+    if (swiperRef !== null && !!activeCard && cardsPerView !== 0) {
+      const slideTo = cardsPerView > 2 ? activeCard - 1 : activeCard;
+      swiperRef.current?.slideTo(slideTo);
     }
-  };
+  }, [swiperRef, cardsWidth, activeCard, cardsPerView]);
 
-  const onClickAction = (index: number) => {
-    setActiveCardIndex((prev) => (prev === index ? null : index));
-    const positionsToCheck = cardsPerView > 2 ? [1, 2] : [0, 1, 2];
-    const lastPosition = positionsToCheck.some(
-      (num) => num === index - position
-    );
-    if (position <= 0 || (lastPosition && !activeCardIndex)) paginate(1);
-    else if (lastPosition && activeCardIndex) paginate(-1);
-  };
+  useEffect(() => {
+    swiperRef.current?.update();
+  }, [cardsPerView]);
 
   return (
-    <section
-      ref={carouselRef}
-      className="relative h-[70vh] flex flex-col items-center justify-center overflow-hidden"
-    >
-      {relatedEvents.map(
-        (
-          {
-            _id,
-            images,
-            title,
-            description,
-            slug,
-            startAt,
-            venue,
-            bookNowUrl,
-            relatedArtists,
-          },
-          index
-        ) => {
-          const cardWidth = oneCardInView
-            ? halfWidth
-            : index === activeCardIndex
-            ? fullWidth
-            : halfWidth;
-
-          const initialPosition =
-            (index + 1 - position) *
-            ((1 / (Math.max(cardsPerView, 2) - 1)) *
-              ((100 / cardsPerView) * (Math.max(cardsPerView, 2) - 1)));
-
-          const positionLeft = oneCardInView
-            ? initialPosition
-            : activeCardIndex !== null && activeCardIndex < index
-            ? initialPosition + (1 / cardsPerView) * 100
-            : initialPosition;
-
-          useEffect(() => {
-            if (position === (cardsPerView - 2) * -1)
-              setDisableSwipingRight(true);
-            else setDisableSwipingRight(false);
-
-            if (position === relatedEvents.length) setDisableSwipingLeft(true);
-            else setDisableSwipingLeft(false);
-          }, [positionLeft, windowWidth, cardsPerView]);
-
-          return (
-            <motion.article
+    // <section
+    //   ref={carouselRef}
+    //   className="relative h-[70vh] flex flex-col items-center justify-center overflow-hidden"
+    // >
+    <section ref={sectionRef} className="mx-5 lg:pb-xxl pb-x">
+      <Swiper
+        grabCursor
+        speed={1000}
+        slidesPerView="auto"
+        spaceBetween={spaceBetween}
+        freeMode={true}
+        modules={[FreeMode]}
+        onBeforeInit={(swiper) => {
+          swiperRef.current = swiper;
+          swiper.update();
+        }}
+        onInit={(swiper) => {
+          setTimeout(() => swiper.update(), 500);
+        }}
+      >
+        {relatedEvents.map(
+          (
+            {
+              _id,
+              images,
+              title,
+              description,
+              slug,
+              startAt,
+              venue,
+              bookNowUrl,
+              relatedArtists,
+            },
+            index
+          ) => (
+            <SwiperSlide
               key={_id}
-              className="flex flex-col overflow-hidden absolute top-0 h-[500px]"
-              initial={{ scale: 0.6 }}
-              animate={{
-                left: `calc(${positionLeft}% - ${gapInPixel}px)`,
-                width: `${cardWidth}%`,
+              onClick={() => onClickCardAction(index)}
+              style={{
+                width: getWidth(index),
               }}
-              style={{ padding: `0 ${gapInPixel}px` }}
-              whileInView={{ scale: 1 }}
-              viewport={{ once: true }}
-              transition={transition(index)}
-              drag="x"
-              dragConstraints={{ left: 0, right: 0 }}
-              dragElastic={0.15}
-              onClick={() => onClickAction(index)}
-              onDragEnd={(_, { offset, velocity }) =>
-                onDragAction(offset, velocity)
-              }
+              className={clsx(
+                "relative aspect-square | transition-all duration-700 ease-in-out | xl:h-[500px] h-[400px]"
+              )}
             >
               <FrontSide
                 index={index}
                 title={title}
                 images={images}
-                active={activeCardIndex === index}
-                cardsPerView={cardsPerView}
                 relatedArtists={relatedArtists}
+                width={cardsWidth! - spaceBetween / 2}
               />
               <Backside
                 description={description}
                 slug={slug}
                 venue={venue}
+                active={index === activeCard}
                 startAt={startAt}
-                active={activeCardIndex === index}
-                cardsPerView={cardsPerView}
-                bookNowUrl={bookNowUrl}
+                width={cardsWidth! - spaceBetween / 2}
               />
-            </motion.article>
-          );
-        }
-      )}
+            </SwiperSlide>
+          )
+        )}
+      </Swiper>
     </section>
   );
 };
