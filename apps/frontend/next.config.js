091789c26a61028260a10f4cad1a68fb4cffc192
reasat-rafio <rafioar96@gmail.com>
2023-02-21 08:53:27 +0600

    :lock: csp update

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -16,7 +16,7 @@ const ContentSecurityPolicy = `
   style-src 'self' 'unsafe-inline' fonts.googleapis.com; 
   base-uri 'self';
   frame-ancestors 'self';
-  frame-src 'self' https://www.youtube.com https://www.onemap.gov.sg www.facebook.com https://singaporebiennale.org;
+  frame-src 'self' https://www.youtube.com https://www.onemap.gov.sg www.facebook.com https://singaporebiennale.org https://www.singaporebiennale.org;
   img-src 'self' cdn.sanity.io 66wl3wil.api.sanity.io https://www.google.com https://www.google-analytics.com https://stats.g.doubleclick.net https://www.facebook.com https://www.google.com.sg; 
   connect-src 'self' https://66wl3wil.api.sanity.io https://www.google-analytics.com https://www.google-analytics.com https://stats.g.doubleclick.net wss://66wl3wil.api.sanity.io https://66wl3wil.api.sanity.io;
   media-src 'self' cdn.sanity.io;
